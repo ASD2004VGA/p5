3d1c778f23a420abbeada412ef477d3df2551a92
petey33 <psterner@gmail.com>
2022-09-23 15:23:34 +0200

    Tilføjet eslint.

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["standard"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "no-undef": false,
+  },
+};
