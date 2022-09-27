a03cc0ed946d2933f47d8f61aaa9aa211b8ed569
petey33 <psterner@gmail.com>
2022-09-27 16:07:32 +0200

    Små ændringer.

--- sketch.js
+++ sketch.js
@@ -1,11 +1,7 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-undef */
-
 function setup () {
   createCanvas(windowWidth, windowHeight)
 }
 
 function draw () {
   background(220)
-  circle(2, 5, 10)
 }
