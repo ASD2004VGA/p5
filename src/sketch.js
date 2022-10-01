8b29a8917f130724a786b12a97b8fa662159a2fa
petey33 <psterner@gmail.com>
2022-10-01 09:23:48 +0200

    Mindre ændringer.

--- src/sketch.js
+++ src/sketch.js
@@ -1,4 +1,6 @@
 
+import * as P5 from 'p5'
+
 const sketch = function (p) {
   const x = 100
   const y = 100
@@ -9,10 +11,9 @@ const sketch = function (p) {
 
   p.draw = function () {
     p.background(0)
-    p.fill('red')
+    p.fill('blue')
     p.rect(x, y, 50, 50)
   }
 }
 
-// eslint-disable-next-line
-const myp5 = new p5(sketch)
+const myp5 = new P5(sketch)
