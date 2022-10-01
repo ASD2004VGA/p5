922add6f103b2a5661acbd1a466b09f9515f9ae5
petey33 <psterner@gmail.com>
2022-10-01 07:28:59 +0200

    Nu virker hot reload med parcel...

--- src/sketch.js
+++ src/sketch.js
@@ -1,8 +1,18 @@
 
-function setup () {
-  createCanvas(windowWidth, windowHeight)
+const sketch = function (p) {
+  const x = 100
+  const y = 100
+
+  p.setup = function () {
+    p.createCanvas(700, 410)
+  }
+
+  p.draw = function () {
+    p.background(0)
+    p.fill('red')
+    p.rect(x, y, 50, 50)
+  }
 }
 
-function draw () {
-  background(220)
-}
+// eslint-disable-next-line
+const myp5 = new p5(sketch)
