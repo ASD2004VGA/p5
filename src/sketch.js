20fd5afe7273350b46715b454d2eba93b8c5f07c
petey33 <psterner@gmail.com>
2022-10-03 10:14:37 +0200

    Oprydning.

--- src/sketch.js
+++ src/sketch.js
@@ -1,19 +1,20 @@
 
-import * as P5 from 'p5'
+import p5 from 'p5'
 
 const sketch = function (p) {
   const x = 100
   const y = 100
 
   p.setup = function () {
-    p.createCanvas(700, 410)
+    p.createCanvas(p.windowWidth, p.windowHeight)
   }
 
   p.draw = function () {
     p.background(0)
-    p.fill('blue')
+    p.fill('red')
     p.rect(x, y, 50, 50)
   }
 }
 
-const myp5 = new P5(sketch)
+// eslint-disable-next-line new-cap, no-new
+new p5(sketch)
