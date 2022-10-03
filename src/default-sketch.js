80ebdf98139aa63d6d47f9114bf6d1e1b02bd1bf
petey33 <psterner@gmail.com>
2022-10-03 13:47:59 +0200

    Nu virker audio og image.

---
+++ src/default-sketch.js
@@ -0,0 +1,31 @@
+import explosionImage from './assets/explosion.png'
+import testAudio from 'url:./assets/audio.mp3'
+
+let img
+let audio
+
+function preload () {
+  img = loadImage(explosionImage)
+}
+
+function setup () {
+  createCanvas(windowWidth, windowHeight)
+  audio = new Audio(testAudio)
+}
+
+function draw () {
+  background(220)
+  circle(windowWidth / 2, windowHeight / 2, 50)
+  image(img, 0, 0)
+}
+
+function mousePressed () {
+  console.log('You clicked the mouse!')
+  audio.play()
+}
+
+function keyPressed () {
+  console.log('You pressed this key: ', key)
+}
+
+export { preload, setup, draw, mousePressed, keyPressed }
--- src/sketch.js
+++ src/sketch.js
@@ -1,7 +1,10 @@
 
 import 'p5'
-import { setup, draw, mousePressed } from './explosion.js'
+// import { setup, draw, mousePressed } from './explosion.js'
+import { preload, setup, draw, mousePressed, keyPressed } from './default-sketch.js'
 
+window.preload = preload
 window.setup = setup
 window.draw = draw
 window.mousePressed = mousePressed
+window.keyPressed = keyPressed
