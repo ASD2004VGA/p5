260b0d118895a55814eedd0d0dce269984798e30
petey33 <psterner@gmail.com>
2022-11-03 14:23:16 +0100

    Default sketch er igen explosion.js.

--- src/sketch.js
+++ src/sketch.js
@@ -1,7 +1,8 @@
 // import { preload, setup, draw, mousePressed, keyPressed } from './default-sketch.js'
-// import { preload, setup, draw, mousePressed, keyPressed } from './explosion.js'
 // import { preload, setup, draw, mousePressed, keyPressed } from './graph.js'
-import { preload, setup, draw, mousePressed, keyPressed } from './matter-js-demo.js'
+// import { preload, setup, draw, mousePressed, keyPressed } from './matter-js-demo.js'
+
+import { preload, setup, draw, mousePressed, keyPressed } from './explosion.js'
 
 window.preload = preload
 window.setup = setup
