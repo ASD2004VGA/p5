d372703741b6222c1f5de4df19aab2b178f6e24a
petey33 <psterner@gmail.com>
2022-11-22 19:00:36 +0100

    Arbejder på dijkstra.

--- src/dijkstra.js
+++ src/dijkstra.js
@@ -17,27 +17,37 @@ class Edge {
 
 const V = []
 const E = []
-const numberOfNodes = 100
+const numberOfNodes = 10
 const numberOfEdges = 2 * numberOfNodes
-
-function findNeighbours () {
-  const neighbours = []
-
-  for (let index = 0; index < array.length; index++) {
-    const element = array[index]
-  }
-
-  return neighbours
-}
 
 function getRandomInt (max) {
   return Math.floor(Math.random() * max)
 }
 
+function findNodeWithSmallestDistance (distances, visited) {
+  const minValue = Infinity
+  let minIndex
+  for (let index = 0; index < visited.length; index++) {
+    if (visited[index]=== false && ) {
+      
+    }
+  }
+}
+
 function dijkstra () {
   const distances = Array(V.length).fill(Infinity)
   const predecessors = Array(V.length).fill(undefined)
-  // source er altid knude på plads 0.
+  // source er altid knuden på plads 0 i V.
+  distances[0] = 0
+
+  const visited = Array(V.length).fill(false)
+  const copyOfV = V.slice()
+
+  const numberOfVisitedNodes = 0
+
+  while (numberOfVisitedNodes < V.length) {
+
+  }
 
   for (let index = 0; index < V.length; index++) {
     const currentNode = V[index]
@@ -53,7 +63,7 @@ function getTwoDifferentRandomNumbers (max) {
     } else if (random1 === 0) {
       random1++
     } else {
-
+      random1++
     }
   }
   return [random1, random2]
@@ -68,7 +78,7 @@ function setup () {
   for (let index = 0; index < numberOfEdges; index++) {
     const [random1, random2] = getTwoDifferentRandomNumbers(V.length)
     const node1 = V[random1]
-    const node2 = V
+    const node2 = V[random2]
     E.push(new Edge(node1, node2, random(0, 100), index))
   }
   createCanvas(windowWidth, windowHeight)
