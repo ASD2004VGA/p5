ec05de418a7c8f88a12c1b11143cf4e12b6243f0
petey33 <psterner@gmail.com>
2022-11-23 15:47:01 +0100

    Nu virker den.

--- src/dijkstra.js
+++ src/dijkstra.js
@@ -1,17 +1,16 @@
 class Node {
-  constructor (x, y, navn) {
+  constructor (x, y, index) {
     this.x = x
     this.y = y
-    this.navn = navn
+    this.index = index
   }
 }
 
 class Edge {
-  constructor (knude1, knude2, vægt, navn) {
+  constructor (knude1, knude2, vægt) {
     this.knude1 = knude1
     this.knude2 = knude2
     this.vægt = vægt
-    this.navn = navn
   }
 }
 
@@ -24,34 +23,50 @@ function getRandomInt (max) {
   return Math.floor(Math.random() * max)
 }
 
-function findNodeWithSmallestDistance (distances, visited) {
-  const minValue = Infinity
+function findUnvisitedNodeWithSmallestDistance (distances, visited) {
+  let minValue = Infinity
   let minIndex
   for (let index = 0; index < visited.length; index++) {
-    if (visited[index]=== false && ) {
-      
+    if (visited[index] === false && distances[index] < minValue) {
+      minValue = distances[index]
+      minIndex = index
+    }
+  }
+  return minIndex
+}
+
+function relax (distances, predecessors, V, E, index) {
+  for (let i = 0; i < E.length; i++) {
+    if (E[i].knude1.index === index) {
+      // Vi har en kant der udgår fra knuden, der er nummereret med index.
+      if (distances[index] + E[i].vægt < distances[E[i].knude2.index]) {
+        distances[E[i].knude2.index] = distances[index] + E[i].vægt
+        predecessors[E[i].knude2.index] = index
+      }
+    } else if (E[i].knude2.index === index) {
+      if (distances[index] + E[i].vægt < distances[E[i].knude1.index]) {
+        distances[E[i].knude1.index] = distances[index] + E[i].vægt
+        predecessors[E[i].knude1.index] = index
+      }
     }
   }
 }
 
-function dijkstra () {
+function dijkstra (V, E) {
   const distances = Array(V.length).fill(Infinity)
   const predecessors = Array(V.length).fill(undefined)
-  // source er altid knuden på plads 0 i V.
-  distances[0] = 0
-
+  distances[0] = 0 // source er altid knuden på plads 0 i V.
   const visited = Array(V.length).fill(false)
-  const copyOfV = V.slice()
 
-  const numberOfVisitedNodes = 0
+  let numberOfVisitedNodes = 0
 
   while (numberOfVisitedNodes < V.length) {
-
+    const index = findUnvisitedNodeWithSmallestDistance(distances, visited)
+    visited[index] = true
+    numberOfVisitedNodes++
+    relax(distances, predecessors, V, E, index)
   }
-
-  for (let index = 0; index < V.length; index++) {
-    const currentNode = V[index]
-  }
+  console.log(distances, predecessors)
 }
 
 function getTwoDifferentRandomNumbers (max) {
@@ -77,19 +92,28 @@ function setup () {
   // generate edges...
   for (let index = 0; index < numberOfEdges; index++) {
     const [random1, random2] = getTwoDifferentRandomNumbers(V.length)
+    console.log(random1, random2)
     const node1 = V[random1]
     const node2 = V[random2]
-    E.push(new Edge(node1, node2, random(0, 100), index))
+    E.push(new Edge(node1, node2, random(0, 100)))
   }
   createCanvas(windowWidth, windowHeight)
   drawGraph()
-  dijkstra()
+
+  dijkstra(V, E)
 }
 
 function drawEdge (e) {
-  fill('red')
-  circle(e.knude1.x, e.knude1.y, 10)
-  circle(e.knude2.x, e.knude2.y, 10)
+  textSize(24)
+  textAlign(CENTER)
+  fill('lightblue')
+  circle(e.knude1.x, e.knude1.y, 40)
+  fill('black')
+  text(e.knude1.index, e.knude1.x, e.knude1.y)
+  fill('lightblue')
+  circle(e.knude2.x, e.knude2.y, 40)
+  fill('black')
+  text(e.knude2.index, e.knude2.x, e.knude2.y)
   line(e.knude1.x, e.knude1.y, e.knude2.x, e.knude2.y)
 }
 
@@ -105,12 +129,4 @@ function draw () {
 
 }
 
-function mousePressed () {
-  console.log('You clicked the mouse!')
-}
-
-function keyPressed () {
-  console.log('You pressed this key: ', key)
-}
-
-export { setup, draw, mousePressed, keyPressed }
+export { setup, draw }
