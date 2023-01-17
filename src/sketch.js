4a67ed9076a299ec97d902c13e5147225fdbbff7
petey33 <psterner@gmail.com>
2023-01-17 13:04:57 +0100

    Solsystem

--- src/sketch.js
+++ src/sketch.js
@@ -2,7 +2,7 @@
 // import { preload, setup, draw, mousePressed, keyPressed } from './graph.js'
 // import { preload, setup, draw, mousePressed, keyPressed } from './matter-js-demo.js'
 
-import { preload, setup, draw, mousePressed, keyPressed } from './dijkstra.js'
+import { preload, setup, draw, mousePressed, keyPressed } from './solsystem.js'
 
 window.preload = preload
 window.setup = setup
